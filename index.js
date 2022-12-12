603b98a2f60d2ceee3e099334e9c4e74a5f14b55
Jayakrishnan <thatcoder.space@gmail.com>
2022-12-12 16:43:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const tracker_api = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello tracker_api`}))
+  res.end()
+  
+}
+
+export default tracker_api
